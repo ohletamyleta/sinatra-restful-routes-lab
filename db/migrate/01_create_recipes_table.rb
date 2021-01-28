3081c4355fc42c2d419fe56576801e74853784cc
ohletamyleta <ohletamyleta@gmail.com>
2021-01-28 09:31:33 -0500

    Done.

---
+++ db/migrate/01_create_recipes_table.rb
@@ -0,0 +1,12 @@
+
+class CreateRecipesTable < ActiveRecord::Migration
+
+  def change
+      create_table :users do |t|
+         t.string :name
+         t.string :ingredients
+         t.string :cook_time
+      end
+  end
+     
+end
